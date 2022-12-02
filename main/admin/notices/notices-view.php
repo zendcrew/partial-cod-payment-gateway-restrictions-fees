ef77b67ca4388894f0e2191ee873ba221714bab8
zendcrew <info@zendcrew.com>
2022-12-02 21:39:43 +0100

    Updated admin notices

--- main/admin/notices/notices-view.php
+++ main/admin/notices/notices-view.php
@@ -1,5 +1,5 @@
 <div class="notice notice-info woopcd-partialcod-notice is-dismissible" data-woopcd_partialcod_id="lite">
-    <p><?php echo wp_kses( __( 'Hey, I noticed you having been using <strong>Paygeo - WooCommerce Partial COD - Gateway Restrictions & Fees</strong>, that’s awesome! Could you please do me a favor and give it a 5-star rating to help us spread the word?', 'woopcd-partialcod' ), array( 'strong' => array() ) ); ?></p>
+    <p><?php echo wp_kses( __( 'Hey, I noticed you having been using <strong>WooCommerce Partial COD - Gateway Restrictions & Fees</strong>, that’s awesome! Could you please do me a favor and give it a 5-star rating to help us spread the word?', 'woopcd-partialcod' ), array( 'strong' => array() ) ); ?></p>
     <p>
         <a href="#" class="woopcd-partialcod-btn" target="_blank"><span class="dashicons dashicons-external"></span><?php echo esc_html__( 'Yes! You deserve it', 'woopcd-partialcod' ); ?></a>
         <a href="#" class="woopcd-partialcod-btn woopcd-partialcod-btn-secondary" data-woopcd_partialcod_remind="yes"><span class="dashicons dashicons-calendar"></span><?php echo esc_html__( 'Nah, maybe later', 'woopcd-partialcod' ); ?></a>
